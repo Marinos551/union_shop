77b34ef21ff1a77ea7351726edcdcc5e2e498643
Marinos Pierides <pieridesmarinos@gmail.com>
2025-11-18 18:53:26 +0000

    Update top banner text to reflect student shop branding

--- lib/main.dart
+++ lib/main.dart
@@ -54,9 +54,9 @@ class HomeScreen extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(vertical: 8),
                     color: const Color(0xFF4d2963),
                     child: const Text(
-                      'PLACEHOLDER HEADER TEXT',
+                      'Union Shop',
                       textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
                     ),
                   ),
                   // Main header
