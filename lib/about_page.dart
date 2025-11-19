11f467a04019ecf7e5361a39b1c3580915c2e0ff
Marinos Pierides <pieridesmarinos@gmail.com>
2025-11-19 04:43:30 +0000

    Add fouter to about page

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/footer_widget.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -50,18 +51,29 @@ class AboutPage extends StatelessWidget {
               style: TextStyle(fontSize: 16),
             ),
             const SizedBox(height: 32),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(16),
-              color: Colors.grey[50],
-              child: const Text(
-                '© 2025 Union Shop. All rights reserved.',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.grey),
-              ),
-            ),
+            // Footer
+            const FooterWidget(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// Added FooterWidget to centralize footer UI
+class FooterWidget extends StatelessWidget {
+  const FooterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      color: Colors.grey[50],
+      child: const Text(
+        '© 2025 Union Shop. All rights reserved.',
+        textAlign: TextAlign.center,
+        style: TextStyle(color: Colors.grey),
       ),
     );
   }
