0e82d9fad0d66f36c7dd24929d183a6629d3b969
Marinos Pierides <pieridesmarinos@gmail.com>
2025-11-19 18:04:31 +0000

    Add product model

--- lib/collection_products_page.dart
+++ lib/collection_products_page.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+// Added Product model here (moved from lib/models/product.dart)
+class Product {
+  final String title;
+  final String price;
+  final String imageUrl;
+  final String category;
+
+  Product({
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+    required this.category,
+  });
+}
+
 class CollectionProductsPage extends StatelessWidget {
   const CollectionProductsPage({super.key});
 
