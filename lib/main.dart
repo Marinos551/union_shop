f131ff647b62f7b9bd2b220dc9e56746e2018c0e
Marinos Pierides <pieridesmarinos@gmail.com>
2025-11-19 16:50:07 +0000

    Add route to main app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:union_shop/product_page.dart';
 import 'package:union_shop/about_page.dart';
 import 'package:union_shop/footer_widget.dart';
+import 'package:union_shop/collections_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
