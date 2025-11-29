67dc68db80d87f3a086720ca9005d924cc3d212d
Marinos Pierides <pieridesmarinos@gmail.com>
2025-11-29 10:11:20 +0000

    Add predefined PrintShackProducts and retrieval function for product details

--- lib/models/print_shack_model.dart
+++ lib/models/print_shack_model.dart
@@ -35,3 +35,34 @@ class PrintShackCustomization {
     return product.basePrice * quantity;
   }
 }
+
+final List<PrintShackProduct> printShackProducts = [
+  PrintShackProduct(
+    id: 'tshirt',
+    name: 'T-Shirt',
+    basePrice: 15.99,
+    availableFonts: ['Arial', 'Helvetica', 'Times New Roman', 'Comic Sans'],
+    availableColors: ['Black', 'White', 'Red', 'Blue', 'Green'],
+    maxTextLength: 30,
+  ),
+  PrintShackProduct(
+    id: 'hoodie',
+    name: 'Hoodie',
+    basePrice: 29.99,
+    availableFonts: ['Arial', 'Helvetica', 'Impact', 'Verdana'],
+    availableColors: ['Black', 'Navy', 'Red', 'Grey'],
+    maxTextLength: 25,
+  ),
+  PrintShackProduct(
+    id: 'mug',
+    name: 'Mug',
+    basePrice: 8.99,
+    availableFonts: ['Arial', 'Script', 'Comic Sans'],
+    availableColors: ['Black', 'Blue', 'Red'],
+    maxTextLength: 20,
+  ),
+];
+
+PrintShackProduct getPrintShackProductById(String id) {
+  return printShackProducts.firstWhere((product) => product.id == id);
+}
