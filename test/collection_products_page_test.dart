4211cf148f44c5fc7ab11be99ac051e2feec43e5
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-01 20:05:05 +0000

    Fix import path for CollectionProductsPage in test file

--- test/collection_products_page_test.dart
+++ test/collection_products_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/collection_products_page.dart';
+import 'package:union_shop/views/collection_products_page.dart';
 
 void main() {
   group('CollectionProductsPage Tests', () {
