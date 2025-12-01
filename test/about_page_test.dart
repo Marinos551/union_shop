e66b13c7e3feadf3ce207bc5ad41e6dca9476565
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-01 20:04:55 +0000

    Fix import path for AboutPage in test file

--- test/about_page_test.dart
+++ test/about_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/about_page.dart';
+import 'package:union_shop/views/about_page.dart';
 
 void main() {
   group('AboutPage Tests', () {
