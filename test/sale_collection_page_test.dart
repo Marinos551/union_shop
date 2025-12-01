c114b2f74236f325ee21c19cd178b60faa86c3a2
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-01 20:05:29 +0000

    Fix import path for SaleCollectionPage in test file

--- test/sale_collection_page_test.dart
+++ test/sale_collection_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/sale_collection_page.dart';
+import 'package:union_shop/views/sale_collection_page.dart';
 
 void main() {
   group('SaleCollectionPage Tests', () {
