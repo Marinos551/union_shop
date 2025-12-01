199e62ed5436960236e1863cb7984ddfbe26fc72
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-01 20:05:17 +0000

    Fix import path for FooterWidget in test file

--- test/footer_widget_test.dart
+++ test/footer_widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/footer_widget.dart';
+import 'package:union_shop/widgets/footer_widget.dart';
 
 void main() {
   group('FooterWidget Tests', () {
